84d612e5ef18c23822ac01d9bdfd84edadf19515
Samuel Silva <samuelsilvapd61@gmail.com>
2021-12-30 16:16:36 -0300

    Initializing action as parameter

--- src/gui/DepartmentListController.java
+++ src/gui/DepartmentListController.java
@@ -40,7 +40,7 @@ public class DepartmentListController implements Initializable{
 		System.out.println("onBtNewAction");
 	}
 	
-	public void setDepartamentService(DepartmentService service) {
+	public void setDepartmentService(DepartmentService service) {
 		this.service = service;
 	}
 	
